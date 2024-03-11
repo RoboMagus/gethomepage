15d5ae783bb05961d13e1d73ac48f0d5e1c1bc05
RoboMagus <68224306+RoboMagus@users.noreply.github.com>
2024-03-11 13:11:43 +0100

    EspHome: Simplify offline devices and add refresh interval

--- docs/widgets/services/esphome.md
+++ docs/widgets/services/esphome.md
@@ -7,10 +7,11 @@ Learn more about [ESPHome](https://esphome.io/).
 
 Show the number of ESPHome devices based on their state.
 
-Allowed fields: `["total", "online", "offline", "unknown"]`.
+Allowed fields: `["total", "online", "offline"]`.
 
 ```yaml
 widget:
   type: esphome
   url: http://esphome.host.or.ip:port
+  refreshInterval: 10000 # optional - in milliseconds, defaults to 30s. Minimal allowed: 10000.
 ```
--- src/widgets/esphome/component.jsx
+++ src/widgets/esphome/component.jsx
@@ -4,11 +4,18 @@ import Block from "components/services/widget/block";
 import Container from "components/services/widget/container";
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
+const defaultInterval = 30000;
+const minRefreshInterval = 10000;
+
 export default function Component({ service }) {
   const { t } = useTranslation();
 
   const { widget } = service;
-  const { data: resultData, error: resultError } = useWidgetAPI(widget);
+  const { refreshInterval = defaultInterval } = widget;
+  const { data: resultData, error: resultError } = useWidgetAPI(widget, "devices", {
+    refreshInterval: Math.max(minRefreshInterval, refreshInterval),
+  });
+
 
   if (resultError) {
     return <Container service={service} error={resultError} />;
@@ -19,7 +26,6 @@ export default function Component({ service }) {
       <Container service={service}>
         <Block label="esphome.online" />
         <Block label="esphome.offline" />
-        <Block label="esphome.unknown" />
         <Block label="esphome.total" />
       </Container>
     );
@@ -27,14 +33,12 @@ export default function Component({ service }) {
 
   const total = Object.keys(resultData).length;
   const online = Object.entries(resultData).filter(([, v]) => v === true).length;
-  const offline = Object.entries(resultData).filter(([, v]) => v === false).length;
-  const unknown = Object.entries(resultData).filter(([, v]) => v === null).length;
+  const offline = Object.entries(resultData).filter(([, v]) => v !== true).length;
 
   return (
     <Container service={service}>
       <Block label="esphome.online" value={t("common.number", { value: online })} />
       <Block label="esphome.offline" value={t("common.number", { value: offline })} />
-      <Block label="esphome.unknown" value={t("common.number", { value: unknown })} />
       <Block label="esphome.total" value={t("common.number", { value: total })} />
     </Container>
   );
